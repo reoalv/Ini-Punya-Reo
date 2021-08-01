3b2d4ec2a18b0b7086e26b9c9d3abb367069cec3
Reo Al Varich <reoalvarich2609@gmail.com>
2021-08-01 15:52:48 +0700

    deleting

--- ToDo/src/Redux/allReducer.js
+++
@@ -1,6 +0,0 @@
-import {combineReducers} from 'redux';
-import ReducerHome from './reducerHome';
-
-export const allReducer = combineReducers({
-  ReducerHome,
-});
--- ToDo/src/Redux/allSaga.js
+++
@@ -1,6 +0,0 @@
-import {all} from 'redux-saga/effects';
-import sagaHome from './sagaHome';
-
-export function* allSaga() {
-  yield all([sagaHome()]);
-}
