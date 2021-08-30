02cf3f5614123069921a94e63a0d790a08e7c2bc
reoalv <reoalvarich2609@gmail.com>
2021-08-30 15:56:36 +0700

    update test element button

--- learnweb/src/elements/Button/index.test.js
+++ learnweb/src/elements/Button/index.test.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import { render } from "@testing-library/react";
 import Button from "./index";
 
@@ -6,4 +7,27 @@ test("Should not allowed click button if isDisabled is present", () => {
   const { container } = render(<Button isDisabled></Button>);
 
   expect(container.querySelector("span.disabled")).toBeInTheDocument();
+});
+
+test("Should render loading/spinner", () => {
+  const { container, getByText } = render(<Button isLoading></Button>);
+
+  expect(getByText(/loading/i)).toBeInTheDocument();
+  expect(container.querySelector("span")).toBeInTheDocument();
+});
+
+test("Should render tag <a>", () => {
+  const { container } = render(<Button isExternal type="link"></Button>);
+
+  expect(container.querySelector("a")).toBeInTheDocument();
+});
+
+test("Should render <Link> component", () => {
+  const { container } = render(
+    <Router>
+      <Button href="" type="link"></Button>
+    </Router>
+  );
+
+  expect(container.querySelector("a")).toBeInTheDocument();
 });
